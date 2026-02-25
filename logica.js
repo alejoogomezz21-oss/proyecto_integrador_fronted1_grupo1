2e79112b7ad92f210e10048088e8254336bb6523
alejoogomezz21-oss <alejoogomezz21@gmail.com>
2026-02-25 14:42:28 -0500

    Definición de variables iniciales para el login

---
+++ logica.js
@@ -0,0 +1,10 @@
+// Fase 1: Estructura de Lógica JavaScript
+// Variables iniciales para el sistema de acceso
+
+const usuarioCorrecto = "estudiante@cesde.edu.co";
+const claveCorrecta = "12345";
+let intentosActuales = 0;
+const limiteDeIntentos = 3;
+
+console.log("Archivo logica.js cargado correctamente.");
+console.log("Esperando interacción del usuario...");
