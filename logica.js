b89ceea3fe56650678f7eee35385fff1831ae657
Juan David <juandalopeztrujillo@gmail.com>
2026-02-26 14:51:05 -0500

    parte integrante #3 en logica.js

--- logica.js
+++ logica.js
@@ -8,7 +8,30 @@ const limiteDeIntentos = 3;
 
 
 function iniciarProceso() {
-    console.log("Iniciando proceso de login institucional...");
+    console.log("Iniciando proceso de ingreso institucional...");
+
+    // APORTE INTEGRANTE 3: Función de Validación y Condicionales
+
+function validarAcceso(usuarioIngresado, claveIngresada) {
+    
+    // 1. Imprimir los datos recibidos por consola
+    console.log(" Proceso de validación ");
+    console.log("Usuario ingresado: " + usuarioIngresado);
+    console.log("Contraseña ingresada: " + claveIngresada);
+    
+    // 2. Verificar mediante condicionales si los datos coinciden
+    // Usamos las variables globales definidas por el Alejo 
+    if (usuarioIngresado === usuarioCorrecto && claveIngresada === claveCorrecta) {
+        alert("Acceso permmitido. Bienvenido");
+        console.log("Resultado: Usuario y clave correctos");
+        return true; // Devuelve verdadero para que el ciclo sepa que puede parar
+    } else {
+        alert(" Datos incorrectos. Intentelo de nuevo.");
+        console.log("Resultado: Usuario y clave incorrectos");
+        return false; // Devuelve falso para que el ciclo cuente un error
+    }
+}
+
 
   
 }
